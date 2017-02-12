5138d857026c66b25598831df500f1b15d97783f
Andrii Los <puha212@gmail.com>
2017-02-12 12:19:57 +0100

    - Added first sql task

---
+++ sql/Tasks.md
@@ -0,0 +1,11 @@
+## SQL tasks
+
+1. Write a query to rank order the following table in MySQL by votes, display the rank as one of the
+columns.
+```sql
+CREATE TABLE votes ( name CHAR(10), votes INT );
+INSERT INTO votes VALUES
+ ('Smith',10), ('Jones',15), ('White',20), ('Black',40), ('Green',50), ('Brown',20);
+ ```
+
+[Solution](/sql/rank.sql)
---
+++ sql/rank.sql
@@ -0,0 +1,15 @@
+/*
+Write a query to rank order the following table in MySQL by votes, display the rank as one of the
+columns.
+*/
+CREATE TABLE votes ( name CHAR(10), votes INT );
+INSERT INTO votes VALUES
+ ('Smith',10), ('Jones',15), ('White',20), ('Black',40), ('Green',50), ('Brown',20);
+
+select
+     @rownum := @rownum + 1 as rank,
+     name,
+     votes
+   from
+      (select @rownum := 0 ) counter,
+      (select name, votes from votes order by votes desc) sorted
