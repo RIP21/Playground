062423fe9450be1e93224fdd729ce4ce8f83b253
Andrii Los <puha212@gmail.com>
2017-03-05 00:38:46 +0100

    - LFUCache task updated

--- java/src/main/java/com/los/LFUCache.java
+++ java/src/main/java/com/los/LFUCache.java
@@ -6,20 +6,23 @@ import java.util.*;
 public class LFUCache<K, V> implements Map<K, V> {
 
     private Map<K, V> cache;
-    private Map<K, Long> accesses;
+    private Queue<Access<K>> accessQueue;
+    private Map<K, Long> accessCounters;
 
     private static final int DEFAULT_CAPACITY = 16;
     private int cacheCapacity;
 
     public LFUCache() {
         cache = new HashMap<>(DEFAULT_CAPACITY, 1);
-        accesses = new LinkedHashMap<>(DEFAULT_CAPACITY, 1);
+        accessCounters = new HashMap<>(DEFAULT_CAPACITY, 1);
+        accessQueue = new PriorityQueue<>();
         this.cacheCapacity = DEFAULT_CAPACITY;
     }
 
     public LFUCache(int cacheCapacity) {
         cache = new HashMap<>(cacheCapacity, 1);
-        accesses = new LinkedHashMap<>(cacheCapacity, 1);
+        accessCounters = new HashMap<>(cacheCapacity, 1);
+        accessQueue = new PriorityQueue<>();
         this.cacheCapacity = cacheCapacity;
     }
 
@@ -62,7 +65,11 @@ public class LFUCache<K, V> implements Map<K, V> {
     }
 
     private void incrementAccess(Object key) {
-        accesses.put((K) key, accesses.get(key) + 1);
+        Long accesses = accessCounters.get(key) + 1;
+        accessCounters.put((K) key, accesses);
+        Access<K> access = new Access<>((K) key, accesses);
+        accessQueue.remove(access);
+        accessQueue.add(access);
     }
 
     @Override
@@ -71,9 +78,10 @@ public class LFUCache<K, V> implements Map<K, V> {
         if (cache.containsKey(key)) {
             return cache.put(key, value);
         } else if (isFull) {
-            removeLeastUsed();
+            cache.remove(accessQueue.poll().getKey());
         }
-        accesses.put(key, 0L);
+        accessQueue.add(new Access<K>(key, 0));
+        accessCounters.put(key, 0L);
         return cache.put(key, value);
     }
 
@@ -91,29 +99,16 @@ public class LFUCache<K, V> implements Map<K, V> {
         m.entrySet().forEach(entry -> put(entry.getKey(), entry.getValue()));
     }
 
-    private void removeLeastUsed() {
-        long min = Long.MAX_VALUE;
-        K leastUsed = null;
-        for (Entry<K, Long> entry : accesses.entrySet()) {
-            if (entry.getValue() < min) {
-                min = entry.getValue();
-                leastUsed = entry.getKey();
-            }
-        }
-        accesses.remove(leastUsed);
-        cache.remove(leastUsed);
-    }
-
     @Override
     public V remove(Object key) {
-        accesses.remove(key);
+        accessQueue.remove(new Access<>((K) key));
         return cache.remove(key);
     }
 
     @Override
     public boolean remove(Object key, Object value) {
         if (cache.remove(key, value)) {
-            accesses.remove(key);
+            accessQueue.remove(new Access<>((K) key));
             return true;
         }
         return false;
@@ -121,7 +116,7 @@ public class LFUCache<K, V> implements Map<K, V> {
 
     @Override
     public void clear() {
-        accesses.clear();
+        accessQueue.clear();
         cache.clear();
     }
 
@@ -140,4 +135,46 @@ public class LFUCache<K, V> implements Map<K, V> {
         return cache.entrySet();
     }
 
+    protected class Access<T> implements Comparable {
+        private T key;
+        private long accesses = 0;
+
+        Access(T key) {
+            this.key = key;
+        }
+
+        Access(T key, long accesses) {
+            this.key = key;
+            this.accesses = accesses;
+        }
+
+        public T getKey() {
+            return key;
+        }
+
+        public long getAccesses() {
+            return accesses;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (getClass() != o.getClass()) return false;
+            Access<?> access = (Access<?>) o;
+            if (this.key == null && this.key == access.getKey()) return true; //case if this.key is null
+
+            return access.key.equals(key);
+        }
+
+        @Override
+        public int compareTo(Object o) {
+            Access<?> obj = (Access<?>) o;
+            if (accesses < obj.getAccesses()) {
+                return -1;
+            } else if (accesses > obj.getAccesses()) {
+                return 1;
+            }
+            return 0;
+        }
+    }
 }
